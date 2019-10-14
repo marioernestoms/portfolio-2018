1b500fbb34ff90f3ac61c0f0810994715e624d31
Mario Ernesto <marioernesto.ms@gmail.com>
2019-10-14 16:40:11 -0300

    update packages, dependences and include bootstrap 4, gulp 4

--- html/gulp/gulpfile.js
+++ html/gulp/gulpfile.js
@@ -15,85 +15,111 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 var config = {
-    node: './node_modules/' ,
-    bootstrap: './node_modules/bootstrap/dist/',
+  node: './node_modules/'
 };
 
-
 // browser-sync task for starting the server.
-gulp.task('browser-sync', function () {
-    //watch files
-    var files = [
-        '../dist/custom.css',
-        '../*.php',
-    ];
+gulp.task('browser-sync', function() {
+  //watch files
+  var files = ['../dist/main.css', '../*.php'];
 
-    //initialize browsersync
-    browserSync.init(files, {
-        //browsersync with a php server
-        proxy: "localhost:8888/portfolio-2018/html",
-        notify: false,
-    });
+  //initialize browsersync
+  browserSync.init(files, {
+    //browsersync with a php server
+    proxy: 'localhost:8888/portfolio-2018/html',
+    notify: false
+  });
 });
 
 // Lint Task
-gulp.task('lint', function () {
-    return gulp.src('../assets/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+gulp.task('lint', function() {
+  return gulp
+    .src('../assets/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
-// Yarn 
-gulp.task('yarn', function () {
-    return gulp.src(['./package.json', './yarn.lock'])
-        .pipe(gulp.dest('./core'))
-        .pipe(yarn({
-            production: true
-        }));
+// Yarn
+gulp.task('yarn', function() {
+  return gulp
+    .src(['./package.json', './yarn.lock'])
+    .pipe(gulp.dest('./core'))
+    .pipe(
+      yarn({
+        production: true
+      })
+    );
 });
 
 // Compile Our Sass
-gulp.task('sass', function () {
-    return gulp.src('../assets/scss/main.scss')
-        .pipe(sass({
-            outputStyle: 'compressed',
-        }))
-        .pipe(gulp.dest('../dist/css/'))
-        .pipe(browserSync.stream());
+gulp.task('sass', function() {
+  return gulp
+    .src([
+      'node_modules/bootstrap/scss/bootstrap.scss',
+      '../assets/scss/main.scss'
+    ])
+    .pipe(
+      sass({
+        outputStyle: 'compressed'
+      })
+    )
+    .pipe(gulp.dest('../dist/css/'))
+    .pipe(browserSync.stream());
 });
 
-/* Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('../assets/js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('../dist/js/'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('../dist/js/'));
+// Concatenate & Minify JS
+gulp.task('js', function() {
+  return gulp
+    .src([
+      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'node_modules/popper.js/dist/popper.min.js',
+      'node_modules/jquery/dist/jquery.min.js'
+    ])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(rename('all.min.js'))
+    .pipe(gulp.dest('../dist/js'))
+    .pipe(browserSync.stream());
 });
-*/
 
-// Compress all images 
-gulp.task('images', function () {
-    return gulp.src('../assets/img/**/*')
-        .pipe(imagemin({
-            progressive: true,
-            interlaced: true,
-            svgoPlugins: [{
-                removeUnknownsAndDefaults: false
-            }, {
-                cleanupIDs: false
-            }]
-        }))
-        .pipe(gulp.dest('../dist/images'))
-        .pipe(browserSync.stream());
+// Compress all images
+gulp.task('images', function() {
+  return gulp
+    .src('../assets/img/**/*')
+    .pipe(
+      imagemin({
+        progressive: true,
+        interlaced: true,
+        svgoPlugins: [
+          {
+            removeUnknownsAndDefaults: false
+          },
+          {
+            cleanupIDs: false
+          }
+        ]
+      })
+    )
+    .pipe(gulp.dest('../dist/images'))
+    .pipe(browserSync.stream());
 });
 
 // Watch Files For Changes
-gulp.task('watch', function () {
-    //gulp.watch('../assets/js/*.js', ['lint', 'scripts']);
-    gulp.watch('../assets/scss/**', ['sass']);
+gulp.task('watch', function() {
+  //gulp.watch('../assets/js/*.js', ['lint', 'scripts']);
+  gulp.watch('../assets/scss/**', ['sass']);
 });
 
+gulp.task(
+  'default',
+  gulp.series(function(done) {
+    // task code here
+    done();
+  })
+);
+
 // Default Task
-gulp.task('default', ['browser-sync', 'lint', 'sass', 'images', 'watch']);
+gulp.task(
+  'default',
+  gulp.series('browser-sync', 'lint', 'sass', 'images', 'watch')
+);
